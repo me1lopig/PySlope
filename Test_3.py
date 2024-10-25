c7194102f7414ee66c95013cc1e890dcd202f2a9
Germán López Pineda <33735247+me1lopig@users.noreply.github.com>
2024-10-25 11:51:54 +0200

    archivos iniciales

---
+++ Test_3.py
@@ -0,0 +1,32 @@
+# Effect of slices on results
+
+import pyslope as sp
+
+
+s = sp.Slope(height=1, angle=None, length=1)
+
+m1 = sp.Material(20, 35, 0, 0.5)
+m2 = sp.Material(20, 35, 0, 1)
+m3 = sp.Material(18, 30, 0, 5)
+
+sp.s.set_materials(m1, m2, m3)
+
+
+
+for r in range(2, 6):
+    s.add_single_circular_plane(
+        c_x=sp.get_bottom_coordinates()[0],
+        c_y=sp.get_bottom_coordinates()[1] + 2.5,
+        radius=r,
+    )
+
+slices = [10, 25, 50, 500, 2000]
+
+for i in slices:
+
+    sp.s.update_analysis_options(slices=i)
+    sp.s.analyse_slope()
+
+    print(f'Slices: {i}')
+    for a in s._search:
+        print(f'radius: {a["radius"]}, FOS: {a["FOS"]}')
