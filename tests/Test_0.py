c9c01c3980322f6d5af5e6414988c95a51d02044
me1lopig <me1lopig@uco.es>
2025-03-09 22:12:26 +0100

    20250309

--- tests/Test_0.py
+++ tests/Test_0.py
@@ -1,25 +1,42 @@
 # Prueba de la libreria pyslope
 
-import pyslope as sp
+import pyslope as psp
 
-s = sp.Slope(height=10, angle=30, length=None)
+s = psp.Slope(height=3, angle=30, length=None)
 
+psp.Slope.plot_boundary(s, material_table=False, legend=False)
 
 # Material defined with key word arguments
-m1 = sp.Material(
+m1 = psp.Material(
     unit_weight=20,
-    friction_angle=25,
-    cohesion=20,
-    depth_to_bottom=15
+    friction_angle=45,
+    cohesion=2,
+    depth_to_bottom=5,
+)
+
+# Material defined with positional arguments
+m2 = psp.Material(20, 30, 2, 3)
+
+# An unlimited number of materials can be assigned at one time
+s.set_materials(m1, m2)
+
+
+# analisys
+s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limits
+
+s.update_analysis_options(
+    slices=50,
+    iterations=2500,
+    tolerance=0.005,
+    max_iterations=50
 )
 
 
-# An unlimited number of materials can be assigned at one time
+s.analyse_slope() # aqui se produce el error
 
-s.set_materials(m1)
+print(s.get_min_FOS())
 
-s.set_analysis_limits(s.get_top_coordinates()[0] - 5, s.get_bottom_coordinates()[0] + 5) # limits
+#s.plot_boundary()  # plots only the boundary
+#s.plot_critical()  # plots the boundary with the critical failure of the slope
+#s.plot_all_planes(max_fos=1) # plots boundary with all slope failures below fos i (where i is number)
 
-s.analyse_slope()
-
-print("FS=",s.get_min_FOS())
