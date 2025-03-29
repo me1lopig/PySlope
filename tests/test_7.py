b086e768892f16def6de7d7a0aa019b31d568de3
German Lopez Pineda <me1lopig@uco.es>
2025-03-29 17:44:12 +0100

    poder ejecutar en consola

--- tests/test_7.py
+++ tests/test_7.py
@@ -5,23 +5,23 @@ from pyslope import Slope, Material
 
 def main():
     # Crear un objeto Slope
-    slope = Slope(height=5, angle=30, length=None)
+    slope = Slope(height=10, angle=30, length=None)
 
     # Definir el primer material
     material1 = Material(
-        unit_weight=20,      # Peso unitario en kN/m3
+        unit_weight=20,      # Peso especifico en kN/m3
         friction_angle=30,   # Ángulo de fricción en grados
         cohesion=10,         # Cohesión en kPa
-        depth_to_bottom=3,   # Profundidad desde la parte superior del talud hasta la base de la capa de material en metro
+        depth_to_bottom=5,   # Espesor desde la parte superior del talud hasta la base de la capa de material en metros
         name="Unidad 1"      # Denominación de la unidad
     )
 
     # Definir el segundo material
     material2 = Material(
-        unit_weight=22,      # Peso unitario en kN/m3
+        unit_weight=22,      # Peso especifico en kN/m3
         friction_angle=35,   # Ángulo de fricción en grados
         cohesion=15,         # Cohesión en kPa
-        depth_to_bottom=6,   # Profundidad desde la parte superior del talud hasta la base de la capa de material en metros
+        depth_to_bottom=6,   # Espesor de la capa medida desde la superior
         name="Unidad 2"      # Denominación de la unidad
     )
 
@@ -29,7 +29,7 @@ def main():
     slope.set_materials(material1, material2)
 
     # Definir el nivel freático
-    slope.set_water_table(4)  # Profundidad del nivel freático desde la parte superior del talud en metros
+    slope.set_water_table(5)  # Profundidad del nivel freático desde la parte superior del talud en metros
 
     # Analizar el talud
     slope.analyse_slope()
@@ -39,8 +39,8 @@ def main():
     print(f"El factor de seguridad mínimo es: {min_fos}")
 
     # Generar y guardar el gráfico del talud crítico
-    fig = slope.plot_critical()
-    fig.write_image("SalidaGrafica.png")
+    #fig = slope.plot_critical()
+    #fig.write_image("SalidaGrafica.png")
 
 if __name__ == "__main__":
     main()
