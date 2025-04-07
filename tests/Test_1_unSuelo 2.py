e8bd99f1d4e5687b4531712a5e1a0fda2efcc3e3
me1lopig <me1lopig@uco.es>
2025-04-07 17:52:25 +0200

    colocacion de peso especifico

--- tests/Test_1_unSuelo 2.py
+++ tests/Test_1_unSuelo 2.py
@@ -13,55 +13,49 @@ def main():
     hoja.append(["Cohesión [kPa]", "fi [º]", "FS"])  
 
     # bucle de cálculo
-    for cohesionTerreno in np.arange(0,50):
-        for anguloRozamiento in np.arange(25,30):
+    for pesoEspecifico in np.arange(14,21):
+        for cohesionTerreno in np.arange(0,51):
+            for anguloRozamiento in np.arange(5,41):
 
-            cohesionTerreno=float(cohesionTerreno)
-            anguloRozamiento=float(anguloRozamiento)
+                pesoEspecifico=float(pesoEspecifico)
+                cohesionTerreno=float(cohesionTerreno)
+                anguloRozamiento=float(anguloRozamiento)
 
-            s = psp.Slope(height=15, angle=45, length=None)
+                s = psp.Slope(height=15, angle=45, length=None)
 
-            psp.Slope.plot_boundary(s, material_table=True, legend=True)
+                psp.Slope.plot_boundary(s, material_table=True, legend=True)
         
-            # Características del material
+                # Características del material
 
-            m1 = psp.Material(
-                unit_weight=18,
-                friction_angle=anguloRozamiento,
-                cohesion=cohesionTerreno,
-                depth_to_bottom=30,
-                name="Unidad 1"
+                m1 = psp.Material(
+                    unit_weight=pesoEspecifico,
+                    friction_angle=anguloRozamiento,
+                    cohesion=cohesionTerreno,
+                    depth_to_bottom=30,
+                    name="Unidad 1"
             )   
 
-            # Asignación del material
-            s.set_materials(m1)
+                # Asignación del material
+                s.set_materials(m1)
 
-            # Cálculo
-            # s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limits
+                # Cálculo
+                # s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limits
 
-            s.update_analysis_options(
-                slices=50,
-                iterations=2500,
-                tolerance=0.005,
-                max_iterations=50
-            )
+                s.update_analysis_options(
+                    slices=50,
+                    iterations=2500,
+                    tolerance=0.005,
+                    max_iterations=50
+                )
 
-            s.analyse_slope()
+                s.analyse_slope()
 
             # Salida de resultados
             #print("Cohesion [kPa]=",cohesionTerreno,"FSmin=",s.get_min_FOS())
 
             # guardado en excel de los resultados de los calculos de una matriz de datos
        
-            hoja.append([cohesionTerreno,anguloRozamiento, s.get_min_FOS()])  
-
-            #s.plot_boundary()  # plots only the boundary
-            #s.plot_critical()  # plots the boundary with the critical failure of the slope
-            #s.plot_all_planes(max_fos=1) # plots boundary with all slope failures below fos i (where i is number)
-
-            # Generar y guardar el gráfico del talud crítico
-            #fig = s.plot_critical()
-            #fig.write_image("SalidaGrafica.png")
+                hoja.append([cohesionTerreno,anguloRozamiento, s.get_min_FOS()])  
 
     # Guardar el archivo Excel  
     nombre_archivo = 'analisis_talud.xlsx'  
