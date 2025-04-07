0e754a829c0f56f6613a88253791f30fcecafab6
Germán López Pineda <me1lopig@uco.es>
2025-04-07 15:15:03 +0200

    20250407

--- tests/Test_1_unSuelo 2.py
+++ tests/Test_1_unSuelo 2.py
@@ -1,26 +1,24 @@
-# Carga de las librerías 
+# Prueba de la libreria pyslope
 
 import pyslope as psp
 import numpy as np
 import openpyxl
 
-# Función principal
+
 def main():
 
     # Creación de la hoja excel en la que volcar los datos
     wb = openpyxl.Workbook()
     hoja = wb.active
-    hoja.append(["Cohesión [kPa]", "fi [º]", "FS"]) # encabezados de los datos
+    hoja.append(["Cohesión [kPa]", "fi [º]", "FS"])  
 
-    # bucle de cálculo con variacion de los patametros resistentes del terreno
-
-    for cohesionTerreno in np.arange(5,101):
-        for anguloRozamiento in np.arange(5,35):
+    # bucle de cálculo
+    for cohesionTerreno in np.arange(0,50):
+        for anguloRozamiento in np.arange(25,30):
 
             cohesionTerreno=float(cohesionTerreno)
             anguloRozamiento=float(anguloRozamiento)
 
-            # Características geoemtricas del talud
             s = psp.Slope(height=15, angle=45, length=None)
 
             psp.Slope.plot_boundary(s, material_table=True, legend=True)
@@ -39,10 +37,10 @@ def main():
             s.set_materials(m1)
 
             # Cálculo
-            # s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limites
+            # s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limits
 
             s.update_analysis_options(
-                slices=25,
+                slices=50,
                 iterations=2500,
                 tolerance=0.005,
                 max_iterations=50
@@ -54,8 +52,16 @@ def main():
             #print("Cohesion [kPa]=",cohesionTerreno,"FSmin=",s.get_min_FOS())
 
             # guardado en excel de los resultados de los calculos de una matriz de datos
+       
             hoja.append([cohesionTerreno,anguloRozamiento, s.get_min_FOS()])  
 
+            #s.plot_boundary()  # plots only the boundary
+            #s.plot_critical()  # plots the boundary with the critical failure of the slope
+            #s.plot_all_planes(max_fos=1) # plots boundary with all slope failures below fos i (where i is number)
+
+            # Generar y guardar el gráfico del talud crítico
+            #fig = s.plot_critical()
+            #fig.write_image("SalidaGrafica.png")
 
     # Guardar el archivo Excel  
     nombre_archivo = 'analisis_talud.xlsx'  
