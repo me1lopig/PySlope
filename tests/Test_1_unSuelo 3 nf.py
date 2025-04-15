5631181229e196a74addfbaf536742929492bba8
me1lopig <me1lopig@uco.es>
2025-04-15 15:53:22 +0200

    20250415

--- tests/Test_1_unSuelo 3 nf.py
+++ tests/Test_1_unSuelo 3 nf.py
@@ -16,47 +16,51 @@ def main():
     # bucle de cálculo
     for altura in np.arange(2,16,2):
         altura=float(altura)
-        for pendiente in np.arange(10,91,5):
-            pendiente=float(pendiente)
-            for pesoEspecifico in np.arange(14,21):
-                pesoEspecifico=float(pesoEspecifico)
-                for cohesionTerreno in np.arange(0,51,5):
-                    cohesionTerreno=float(cohesionTerreno)
-                    for anguloRozamiento in np.arange(5,41,5):
-                        anguloRozamiento=float(anguloRozamiento)
+        for nf in np.arange(0,altura+altura*0.25,0.5):
+            for pendiente in np.arange(10,91,5):
+                pendiente=float(pendiente)
+                for pesoEspecifico in np.arange(14,21):
+                    pesoEspecifico=float(pesoEspecifico)
+                    for cohesionTerreno in np.arange(0,51,5):
+                        cohesionTerreno=float(cohesionTerreno)
+                        for anguloRozamiento in np.arange(5,41,5):
+                            anguloRozamiento=float(anguloRozamiento)
 
-                        s = psp.Slope(height=altura, angle=pendiente, length=None)
+                            s = psp.Slope(height=altura, angle=pendiente, length=None)
 
-                        psp.Slope.plot_boundary(s, material_table=True, legend=True)
+                            psp.Slope.plot_boundary(s, material_table=True, legend=True)
         
-                        # Características del material
+                            # Características del material
 
-                        m1 = psp.Material(
-                            unit_weight=pesoEspecifico,
-                            friction_angle=anguloRozamiento,
-                            cohesion=cohesionTerreno,
-                            depth_to_bottom=altura*2,
-                            name="Unidad 1"
-                        )   
+                            m1 = psp.Material(
+                                unit_weight=pesoEspecifico,
+                                friction_angle=anguloRozamiento,
+                                cohesion=cohesionTerreno,
+                                depth_to_bottom=altura*2,
+                                name="Unidad 1"
+                            )   
 
-                        # Asignación del material
-                        s.set_materials(m1)
+                            # Asignación del material
+                            s.set_materials(m1)
 
-                        # Cálculo
-                        # s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limits
+                            # Definir el nivel freático
+                            s.set_water_table(nf)  # Profundidad del nivel freático desde la parte superior del talud en metros
 
-                        s.update_analysis_options(
-                            slices=50,
-                            iterations=2500,
-                            tolerance=0.005,
-                            max_iterations=50
-                        )
+                            # Cálculo
+                            # s.set_analysis_limits(s.get_top_coordinates()[0] - 10, s.get_bottom_coordinates()[0] + 10) # limits
 
-                        s.analyse_slope()
+                            s.update_analysis_options(
+                                slices=50,
+                                iterations=2500,
+                                tolerance=0.005,
+                                max_iterations=50
+                            )
 
-                    # guardado en excel de los resultados de los calculos de una matriz de datos
+                            s.analyse_slope()
+
+                        # guardado en excel de los resultados de los calculos de una matriz de datos
        
-                        hoja.append([altura,pendiente,pesoEspecifico,cohesionTerreno,anguloRozamiento, s.get_min_FOS()])  
+                            hoja.append([altura,pendiente,pesoEspecifico,cohesionTerreno,anguloRozamiento, s.get_min_FOS()])  
 
     # Guardar el archivo Excel  
     nombre_archivo = 'analisis_talud.xlsx'  
